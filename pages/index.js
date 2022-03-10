85d8842705a6b6528dc38af122722ae6cbee4c50
Walter van Woudenberg <vanwoudenbergwalter@gmail.com>
2022-03-10 17:22:32 +0100

    comment images

--- pages/index.js
+++ pages/index.js
@@ -158,7 +158,7 @@ export default function Home(props) {
               </div>
             </div>
           </div>
-        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4'>
+        {/* <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4'>
 
           {
             nfts.map((nft, i) => (
@@ -197,7 +197,7 @@ export default function Home(props) {
             ))
           }
 
-        </div>
+        </div> */}
 
       </div>
 
