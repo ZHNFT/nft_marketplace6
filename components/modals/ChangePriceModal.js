3bdaf17ea0ecf49625816d40915401f0d088026e
ange <blockchainang@gmail.com>
2022-03-15 15:40:25 +1100

    change price

--- components/modals/ChangePriceModal.js
+++ components/modals/ChangePriceModal.js
@@ -1,78 +1,47 @@
-import { useForm } from '../../hooks/useForm';
+import { useState } from 'react';
+import { TRANSACTION_STATUS } from '../../constants/nft';
+import TransactionList from '../Transactions/TransactionList';
 import Modal from './Modal';
-import PrimaryButton from '../Buttons/PrimaryButton';
-import PrimaryAltButton from '../Buttons/PrimaryAltButton';
-import Dropdown from '../Dropdown/Dropdown';
+import ChangePriceForm from '../Forms/ChangePriceForm';
 
 const currencies = [
   { label: 'HNY', value: 'hny' }
 ];
 
 export default function ChangePriceModal({ isOpen, onClose, onConfirm }) {
-  const {
-    handleSubmit, // handles form submission
-    handleChange, // handles input changes
-    data, // access to the form data
-    errors, // includes the errors to show
-  } = useForm({
-    validations: { // validation rules
-      price: {
-        pattern: {
-          value: '^(0*[1-9][0-9]*(\.[0-9]+)?|0+\.[0-9]*[1-9][0-9]*)$',
-          message: 'Invalid amount'
-        },
-      },
-    },
-    onSubmit: () => {
-      onConfirm({
-        ...data,
-        currency: data.currency?.value
-      });
-    },
-    initialValues: { // used to initialize the data
-      currency: currencies[0],
-      price: ''
-    },
-  });
-
+  const [formData, setFormData] = useState(null);
   return (
     <Modal isOpen={isOpen} onClose={onClose} title="Lower the listing price">
-      <div className="mt-4 mb-6">
-        <label htmlFor="price" className="sr-only">Price</label>
-        <div className="mt-1 relative">
-          <div className="flex">
-            <Dropdown
-              label={ data.type === 'fixed' ? 'Price' : 'Starting price' }
-              className="mr-4 max-w-[128px] text-base"
-              isDisabled
-              selected={data.currency}
-              onSelect={selected => {
-                handleChange('currency')({ target: { value: selected }});
-              }}
-              list={currencies}
+      {
+        formData
+          ? (
+            <TransactionList
+              steps={[
+                {
+                  title: 'Transaction to cancel old price',
+                  status: TRANSACTION_STATUS.IN_PROGRESS,
+                  isDefaultOpen: true,
+                  description: 'Description here'
+                },
+                {
+                  className: 'my-2',
+                  title: 'Requesting signature',
+                  status: TRANSACTION_STATUS.INACTIVE,
+                  isDefaultOpen: false,
+                  description: 'Description here'
+                },
+                {
+                  className: 'my-2',
+                  title: 'Completion',
+                  status: TRANSACTION_STATUS.INACTIVE,
+                  isDefaultOpen: false,
+                  description: 'Description here'
+                }
+              ]}
             />
-            <input
-              type="text"
-              id="price"
-              name="price"
-              className="text-ink rounded-xl flex flex-1"
-              value={data.price || ''}
-              onChange={handleChange('price')}
-              required
-            />
-          </div>
-          { errors.price && <p className="mt-1 absolute text-red-600">{ errors.price }</p> }
-        </div>
-      </div>
-
-      <p className="text-justify mt-10">
-        You must pay an additional gas fee if you want to cancel this listing at a later point. <a href="#" className="text-cornflower">Learn more about canceling listings.</a>
-      </p>
-
-      <div className="flex justify-center mt-10 my-4">
-        <PrimaryButton className="max-w-[200px]" onClick={handleSubmit}>Set new price</PrimaryButton>
-        <PrimaryAltButton className="ml-4 max-w-[200px]" onClick={onClose}>Never mind</PrimaryAltButton>
-      </div>
+          )
+          : <ChangePriceForm onClose={onClose} onConfirm={setFormData} />
+      }
     </Modal>
   );
 }
