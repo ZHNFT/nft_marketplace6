5ba8d9956878b6f7513528c05a57f27cca4a5f04
Walter van Woudenberg <vanwoudenbergwalter@gmail.com>
2022-03-17 18:51:39 +0100

    remove

--- pages/collections/[address]/token/[id].js
+++ pages/collections/[address]/token/[id].js
@@ -25,10 +25,6 @@ import Activity from '../../../../components/Product/Activity';
 import ItemPrice from '../../../../components/ItemPrice/ItemPrice';
 import TraitsTable from '../../../../components/Traits/TraitsTable';
 
-function generateActivityData(data) {
-  const { auctions, listings, transfers, bids } = data;
-}
-
 // This will be the Single Asset of a collection (Single NFT)
 // Route: http://localhost:3000/collection/[address]/[id]
 // Example: http://localhost:3000/collection/0xdbe147fc80b49871e2a8d60cc89d51b11bc88b35/198
