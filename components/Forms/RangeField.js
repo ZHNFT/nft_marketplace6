7db91e21fa48027fc2a5ac30e96e06de9ea7c887
ange <blockchainang@gmail.com>
2022-03-21 22:19:35 +1100

    added RangeField component

---
+++ components/Forms/RangeField.js
@@ -0,0 +1,63 @@
+import { useState, useEffect } from 'react';
+import { Range, getTrackBackground } from 'react-range';
+
+export default function RangeField({ step = 0.01, decimals, min = 0, max = 100, onChange }) {
+  const [values, setValues] = useState([0, 0]);
+
+  useEffect(() => {
+    setValues([min, max]);
+  }, [min, max]);
+
+  return (
+    <div className="flex justify-center">
+      <Range
+        values={values}
+        step={step}
+        min={min}
+        max={max}
+        onChange={values => { setValues(values); onChange(values); }}
+        renderTrack={({ props, children }) => (
+          <div
+            onMouseDown={props.onMouseDown}
+            onTouchStart={props.onTouchStart}
+            style={{
+              ...props.style,
+              height: '36px',
+              display: 'flex',
+              width: '95%'
+            }}
+          >
+            <div
+              ref={props.ref}
+              className="h-[2px] w-full self-center"
+              style={{
+                background: getTrackBackground({
+                  values,
+                  colors: ['#494433', '#4d71be', '#494433'],
+                  min,
+                  max
+                })
+              }}
+            >
+              {children}
+            </div>
+          </div>
+        )}
+        renderThumb={({ index, props, isDragged }) => (
+          <div
+            {...props}
+            className="w-[12px] h-[12px] rounded-full flex justify-center items-center"
+            style={{
+              ...props.style,
+              backgroundColor: isDragged ? '#5a93ff' : '#4d71be'
+            }}
+          >
+            <div className="absolute -bottom-[30px] py-1 px-1.5 dark:bg-white/[0.1] bg-black/[0.1] rounded text-xs">
+              { values[index] === 0 || values[index] === max ? values[index] : values[index].toFixed(decimals) }
+            </div>
+          </div>
+        )}
+      />
+    </div>
+  );
+};
