17d72c300856c6ac4c2c94deacf8082b187c325d
nolifechew <haunterkirby@gmail.com>
2022-04-01 13:28:07 -0700

    activity icon update

--- components/icons/BeeIcon.js
+++ components/icons/BeeIcon.js
@@ -2,7 +2,7 @@
 export default function BeeIcon({ className }) {
     return ( 
         <svg className={`inline-block ${className}`} viewBox="0 0 140 166" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <g clip-path="url(#clip0_2118_261)">
+        <g clipPath="url(#clip0_2118_261)">
         <path d="M70 101.26C108.66 101.26 140 78.5922 140 50.63C140 22.6678 108.66 0 70 0C31.3401 0 0 22.6678 0 50.63C0 78.5922 31.3401 101.26 70 101.26Z" fill="url(#paint0_radial_2118_261)"/>
         <path d="M70 165.68C108.66 165.68 140 134.34 140 95.6799C140 57.02 108.66 25.6799 70 25.6799C31.3401 25.6799 0 57.02 0 95.6799C0 134.34 31.3401 165.68 70 165.68Z" fill="url(#paint1_radial_2118_261)"/>
         <path d="M51.4599 26.79C46.4899 28.41 41.8 30.85 37.56 34.02C32.07 37.67 28.44 43.72 27.7 50.48C28.42 57.27 32.04 63.35 37.56 67.01C41.8 70.17 46.4899 72.61 51.4599 74.24C57.4599 76.21 63.71 77.25 70 77.32C76.25 77.32 82.4799 76.37 88.4599 74.47C93.4599 72.86 98.18 70.42 102.44 67.24C108.01 63.53 111.64 57.35 112.3 50.47C111.56 43.71 107.94 37.66 102.44 34.01C98.18 30.83 93.4599 28.39 88.4599 26.78C82.5199 24.88 76.3499 23.92 70.1399 23.93C65.1899 23.93 60.26 24.51 55.43 25.62L51.4599 26.77V26.79Z" fill="url(#paint2_radial_2118_261)"/>
@@ -10,38 +10,38 @@ export default function BeeIcon({ className }) {
         </g>
         <defs>
         <radialGradient id="paint0_radial_2118_261" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(68.78 35.83) rotate(90) scale(50.53 75.795)">
-        <stop stop-color="#FFCD50" stop-opacity="0.2"/>
-        <stop offset="0.24" stop-color="#FFCD50" stop-opacity="0.21"/>
-        <stop offset="0.43" stop-color="#FFCD50" stop-opacity="0.25"/>
-        <stop offset="0.6" stop-color="#FFCD50" stop-opacity="0.32"/>
-        <stop offset="0.77" stop-color="#FFCD50" stop-opacity="0.41"/>
-        <stop offset="0.92" stop-color="#FFCD50" stop-opacity="0.53"/>
-        <stop offset="1" stop-color="#FFCD50" stop-opacity="0.6"/>
+        <stop stopColor="#FFCD50" stopOpacity="0.2"/>
+        <stop offset="0.24" stopColor="#FFCD50" stopOpacity="0.21"/>
+        <stop offset="0.43" stopColor="#FFCD50" stopOpacity="0.25"/>
+        <stop offset="0.6" stopColor="#FFCD50" stopOpacity="0.32"/>
+        <stop offset="0.77" stopColor="#FFCD50" stopOpacity="0.41"/>
+        <stop offset="0.92" stopColor="#FFCD50" stopOpacity="0.53"/>
+        <stop offset="1" stopColor="#FFCD50" stopOpacity="0.6"/>
         </radialGradient>
         <radialGradient id="paint1_radial_2118_261" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(70 25.9299) scale(137)">
-        <stop stop-color="#FFCD50" stop-opacity="0.2"/>
-        <stop offset="0.19" stop-color="#FFCD50" stop-opacity="0.22"/>
-        <stop offset="0.39" stop-color="#FFCD50" stop-opacity="0.27"/>
-        <stop offset="0.6" stop-color="#FFCD50" stop-opacity="0.36"/>
-        <stop offset="0.82" stop-color="#FFCD50" stop-opacity="0.47"/>
-        <stop offset="1" stop-color="#FFCD50" stop-opacity="0.6"/>
+        <stop stop-color="#FFCD50" stopOpacity="0.2"/>
+        <stop offset="0.19" stopColor="#FFCD50" stopOpacity="0.22"/>
+        <stop offset="0.39" stopColor="#FFCD50" stopOpacity="0.27"/>
+        <stop offset="0.6" stopColor="#FFCD50" stopOpacity="0.36"/>
+        <stop offset="0.82" stopColor="#FFCD50" stopOpacity="0.47"/>
+        <stop offset="1" stopColor="#FFCD50" stopOpacity="0.6"/>
         </radialGradient>
         <radialGradient id="paint2_radial_2118_261" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(70.0496 94.26) scale(68.0064 70.84)">
-        <stop stop-color="#FFE48F" stop-opacity="0.6"/>
-        <stop offset="0.08" stop-color="#FFE181" stop-opacity="0.68"/>
-        <stop offset="0.23" stop-color="#FFDD6D" stop-opacity="0.8"/>
-        <stop offset="0.39" stop-color="#FFD95D" stop-opacity="0.89"/>
-        <stop offset="0.56" stop-color="#FFD752" stop-opacity="0.95"/>
-        <stop offset="0.75" stop-color="#FFD64C" stop-opacity="0.99"/>
-        <stop offset="1" stop-color="#FFD64A"/>
+        <stop stopColor="#FFE48F" stopOpacity="0.6"/>
+        <stop offset="0.08" stopColor="#FFE181" stopOpacity="0.68"/>
+        <stop offset="0.23" stopColor="#FFDD6D" stopOpacity="0.8"/>
+        <stop offset="0.39" stopColor="#FFD95D" stopOpacity="0.89"/>
+        <stop offset="0.56" stopColor="#FFD752" stopOpacity="0.95"/>
+        <stop offset="0.75" stopColor="#FFD64C" stopOpacity="0.99"/>
+        <stop offset="1" stopColor="#FFD64A"/>
         </radialGradient>
         <radialGradient id="paint3_radial_2118_261" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(70.0496 95.0901) scale(68.0064 70.84)">
         <stop stop-color="#FFB000"/>
-        <stop offset="0.18" stop-color="#FFB206" stop-opacity="0.98"/>
-        <stop offset="0.39" stop-color="#FFB919" stop-opacity="0.93"/>
-        <stop offset="0.6" stop-color="#FFC539" stop-opacity="0.84"/>
-        <stop offset="0.83" stop-color="#FFD566" stop-opacity="0.71"/>
-        <stop offset="1" stop-color="#FFE48F" stop-opacity="0.6"/>
+        <stop offset="0.18" stopColor="#FFB206" stopOpacity="0.98"/>
+        <stop offset="0.39" stopColor="#FFB919" stopOpacity="0.93"/>
+        <stop offset="0.6" stopColor="#FFC539" stopOpacity="0.84"/>
+        <stop offset="0.83" stopColor="#FFD566" stopOpacity="0.71"/>
+        <stop offset="1" stopColor="#FFE48F" stopOpacity="0.6"/>
         </radialGradient>
         <clipPath id="clip0_2118_261">
         <rect width="140" height="165.68" fill="white"/>
--- components/icons/TransferIcon.js
+++ components/icons/TransferIcon.js
@@ -2,10 +2,10 @@
 export default function TransferIcon({ className }) {
     return ( 
         <svg className={`inline-block ${className}`} viewBox="0 0 20 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M4.36572 13.165L1.00003 9.79929L4.36572 6.43359" stroke-linecap="round" stroke-linejoin="round" fill="currentColor" stroke="currentColor"/>
-        <path d="M1.00003 9.79904H11.4018" stroke-linecap="round" stroke-linejoin="round" fill="currentColor" stroke="currentColor"/>
-        <path d="M15.896 1.83477L19.2617 5.20046L15.896 8.56616" stroke-linecap="round" stroke-linejoin="round" fill="currentColor" stroke="currentColor"/>
-        <path d="M19.2614 5.20096H8.85965" stroke-linecap="round" stroke-linejoin="round" fill="currentColor" stroke="currentColor"/>
+        <path d="M4.36572 13.165L1.00003 9.79929L4.36572 6.43359" strokeLinecap="round" strokeLinejoin="round" fill="currentColor" stroke="currentColor"/>
+        <path d="M1.00003 9.79904H11.4018" strokeLinecap="round" strokeLinejoin="round" fill="currentColor" stroke="currentColor"/>
+        <path d="M15.896 1.83477L19.2617 5.20046L15.896 8.56616" strokeLinecap="round" strokeLinejoin="round" fill="currentColor" stroke="currentColor"/>
+        <path d="M19.2614 5.20096H8.85965" strokeLinecap="round" strokeLinejoin="round" fill="currentColor" stroke="currentColor"/>
         </svg>
     );
   }
