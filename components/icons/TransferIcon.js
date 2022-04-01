52422de60d0868594a40d2a08722148a04d0ea50
nolifechew <haunterkirby@gmail.com>
2022-04-01 12:39:20 -0700

    added activity icons

---
+++ components/icons/TransferIcon.js
@@ -0,0 +1,10 @@
+export default function TransferIcon({ className }) {
+    return ( 
+        <svg width="20" height="15" viewBox="0 0 20 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M4.36572 13.165L1.00003 9.79929L4.36572 6.43359" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M1.00003 9.79904H11.4018" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M15.896 1.83477L19.2617 5.20046L15.896 8.56616" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M19.2614 5.20096H8.85965" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+    );
+  }
