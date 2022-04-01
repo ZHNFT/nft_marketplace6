90b4fed321a8955bdb4c6b299ab00ceb87a9da47
nolifechew <haunterkirby@gmail.com>
2022-04-01 12:57:18 -0700

    icons update

--- components/icons/MintIcon.js
+++ components/icons/MintIcon.js
@@ -1,6 +1,7 @@
+
 export default function MintIcon({ className }) {
     return ( 
-        <svg width="17" height="19" viewBox="0 0 17 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg className={`inline-block ${className}`} viewBox="0 0 17 19" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M12.1198 7.26376L4.49951 2.86938" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
         <path d="M15.9301 12.8521V6.07847C15.9298 5.78151 15.8514 5.48985 15.7028 5.23275C15.5542 4.97565 15.3405 4.76216 15.0834 4.61368L9.15646 1.22687C8.89903 1.07825 8.60702 1 8.30976 1C8.01251 1 7.72049 1.07825 7.46306 1.22687L1.53615 4.61368C1.27898 4.76216 1.06537 4.97565 0.91676 5.23275C0.768148 5.48985 0.689758 5.78151 0.689453 6.07847V12.8521C0.689758 13.149 0.768148 13.4407 0.91676 13.6978C1.06537 13.9549 1.27898 14.1684 1.53615 14.3169L7.46306 17.7037C7.72049 17.8523 8.01251 17.9305 8.30976 17.9305C8.60702 17.9305 8.89903 17.8523 9.15646 17.7037L15.0834 14.3169C15.3405 14.1684 15.5542 13.9549 15.7028 13.6978C15.8514 13.4407 15.9298 13.149 15.9301 12.8521Z" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
         <path d="M0.918213 5.19788L8.30991 9.47372L15.7016 5.19788" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
