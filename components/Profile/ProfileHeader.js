99a89ef2bff42c9ce25985d003dcd5e19bcae6b3
0xB1lly <billyk@dehedge.fund>
2022-04-03 14:08:58 -0500

    feat: dynamic volume and est.value on profile

--- components/Profile/ProfileHeader.js
+++ components/Profile/ProfileHeader.js
@@ -10,13 +10,23 @@ import {
 } from "../icons";
 import { getExplorer } from "../../config";
 import { ellipseAddress } from "../../Utils";
+import { formatEther, formatCompact } from "../../Utils/helper";
 import SecondaryButton from "../Buttons/SecondaryButton";
 import AppGlobalContext from "../../contexts/AppGlobalContext";
 import DefaultLogo from "../../images/default-collection-logo.png";
-import HNYicon from "../../images/icon-hny.png";
+import Web3Context from "../../contexts/Web3Context";
 
-export default function ProfileHeader({ chainIdHex, userData, address, total }) {
+export default function ProfileHeader({
+  chainIdHex,
+  userData,
+  address,
+  total,
+}) {
   const { setShowEditProfileModal } = useContext(AppGlobalContext);
+  const {
+    state: { tokenData },
+  } = useContext(Web3Context);
+  const tokenPriceUsd = tokenData?.priceUsd;
 
   return (
     <>
@@ -75,12 +85,12 @@ export default function ProfileHeader({ chainIdHex, userData, address, total }) 
                 </li>
               )}
               {
-              // <li>
-              //   <a href="#">
-              //     <span className="hidden">Share</span>
-              //     <ShareIcon className="w-[14px]" />
-              //   </a>
-              // </li>
+                // <li>
+                //   <a href="#">
+                //     <span className="hidden">Share</span>
+                //     <ShareIcon className="w-[14px]" />
+                //   </a>
+                // </li>
               }
             </ul>
           </div>
@@ -98,15 +108,21 @@ export default function ProfileHeader({ chainIdHex, userData, address, total }) 
             <li>
               <h4 className="text-xs mb-1.5">Volume</h4>
               <span className="text-xl font-medium">
-                <BeeIcon className="h-[17px] relative -top-[2px] pr-[5px]"/>
-                16.7K
+                <BeeIcon className="h-[17px] relative -top-[2px] pr-[5px]" />
+                {userData.volume?.total
+                  ? formatEther(userData.volume.total)
+                  : 0}
               </span>
             </li>
             <li>
               <h4 className="text-xs mb-1.5">Est. Value</h4>
               <span className="text-xl font-medium">
-                <BeeIcon className="h-[17px] relative -top-[2px] pr-[5px]"/>
-                16.7K
+                ${userData.estimatedValue && tokenPriceUsd
+                  ? formatCompact(
+                      Number(formatEther(userData.estimatedValue)) *
+                        Number(tokenPriceUsd)
+                    )
+                  : 0}
               </span>
             </li>
           </ul>
