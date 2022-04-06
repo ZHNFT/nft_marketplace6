4fc8830912bc0797cc1ecba98896ec8806aea672
Walter van Woudenberg <vanwoudenbergwalter@gmail.com>
2022-04-06 10:33:10 +0200

    update seo component

--- components/seo.js
+++ components/seo.js
@@ -3,10 +3,10 @@ import Head from 'next/head'
 export default function Navbar(props) {
   return (
     <Head>
-      <title>Hexagon NFT Marketplace</title>
-      <meta name="description" content="The Hexagon NFT Marketplace is part of the hive.investments ecosystem running on polygon." />
-      <meta property="og:title" content="Hexagon NFT Marketplace" />
-      <meta property="og:description" content="The Hexagon NFT Marketplace is part of the hive.investments ecosystem running on polygon." />
+      <title>Hexagon</title>
+      <meta name="description" content="A next gen premium NFT marketplace on Polygon with only the most premier NFT projects. Buy, sell, and auction hand-vetted NFT collections seamlessly with a vibrant community of investors, creators, and crypto enthusiasts." />
+      <meta property="og:title" content="Hexagon" />
+      <meta property="og:description" content="A next gen premium NFT marketplace on Polygon with only the most premier NFT projects. Buy, sell, and auction hand-vetted NFT collections seamlessly with a vibrant community of investors, creators, and crypto enthusiasts." />
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://hexag0n.io/" />
       <meta property="og:image" content="" />
@@ -14,8 +14,8 @@ export default function Navbar(props) {
       <meta name="twitter:creator" content="@hiveinvestments" />
       <meta name="twitter:site" content="@hiveinvestments" />
       <meta name="twitter:card" content="summary" /> 
-      <meta name="twitter:title" content="Hexagon NFT Marketplace" />
-      <meta name="twitter:description" content="The Hexagon NFT Marketplace is part of the hive.investments ecosystem running on polygon." />
+      <meta name="twitter:title" content="Hexagon" />
+      <meta name="twitter:description" content="A next gen premium NFT marketplace on Polygon with only the most premier NFT projects. Buy, sell, and auction hand-vetted NFT collections seamlessly with a vibrant community of investors, creators, and crypto enthusiasts." />
       <link rel="icon" href="/favicon.ico" />
     </Head>
   );
