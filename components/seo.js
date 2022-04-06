49f939790262c2ca5a92e88947bef6e873f3facd
Walter van Woudenberg <vanwoudenbergwalter@gmail.com>
2022-04-06 03:47:43 +0200

    update seo for the time being

--- components/seo.js
+++ components/seo.js
@@ -3,19 +3,19 @@ import Head from 'next/head'
 export default function Navbar(props) {
   return (
     <Head>
-      <title>hive investments</title>
-      <meta name="description" content="Introducing #HIVE. A Polygon based #DaaS project built by bees, for bees to take over #DeFi. Proudly a core protocol of the Union of Transparency #UoT." />
-      <meta property="og:title" content="" />
-      <meta property="og:description" content="Introducing #HIVE. A Polygon based #DaaS project built by bees, for bees to take over #DeFi. Proudly a core protocol of the Union of Transparency #UoT." />
+      <title>Hexagon NFT Marketplace</title>
+      <meta name="description" content="The Hexagon NFT Marketplace is part of the hive.investments ecosystem running on polygon." />
+      <meta property="og:title" content="Hexagon NFT Marketplace" />
+      <meta property="og:description" content="The Hexagon NFT Marketplace is part of the hive.investments ecosystem running on polygon." />
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="" />
+      <meta property="og:url" content="https://hexag0n.io/" />
       <meta property="og:image" content="" />
       <meta name="twitter:image" content="" />
       <meta name="twitter:creator" content="@hiveinvestments" />
       <meta name="twitter:site" content="@hiveinvestments" />
       <meta name="twitter:card" content="summary" /> 
-      <meta name="twitter:title" content="hive investments" />
-      <meta name="twitter:description" content="Introducing #HIVE. A Polygon based #DaaS project built by bees, for bees to take over #DeFi. Proudly a core protocol of the Union of Transparency #UoT." />
+      <meta name="twitter:title" content="Hexagon NFT Marketplace" />
+      <meta name="twitter:description" content="The Hexagon NFT Marketplace is part of the hive.investments ecosystem running on polygon." />
       <link rel="icon" href="/favicon.ico" />
     </Head>
   );
